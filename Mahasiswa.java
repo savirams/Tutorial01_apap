0c0e5093257a5e041dad496c5191d9a82f815a29
Savira Maharani Santoso <maharanisavira29@gmail.com>
2018-09-07 17:00:25 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,21 @@
+
+public class Mahasiswa {
+	int semester;
+	String name;
+	public Mahasiswa(int semester, String name) {
+		this.semester = semester;
+		this.name = name;
+	}
+	public int getSemester() {
+		return semester;
+	}
+	public void setSemester(int semester) {
+		this.semester = semester;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
